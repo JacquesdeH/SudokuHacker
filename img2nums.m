7b0147c38df78bebff79891806bf22a83ff50f95
XBR-1111 <59074275+XBR-1111@users.noreply.github.com>
2020-12-07 23:38:30 +0800

    Add files via upload

--- img2nums.m
+++ img2nums.m
@@ -1,14 +1,15 @@
-function [digit_pics,is_empty]  = img2nums(filename)
+%function [digit_pics,is_empty]  = img2nums(filename)
 %输入：图片的名称，如果不在当前文件夹下可添加路径
-%输出：digit_pics为81*28*28的矩阵，包含81个28*28的数字图片。digit1 = reshape(digit_pics(1,:),28,28)
+%输出：digit_pics为28*28*81的矩阵，包含81个28*28的数字图片。digit1 = reshape(digit_pics(1,:),28,28)
 %       is_empty 为9*9矩阵，1为空 0为非空
 %手动标记部分1：数独的4个角，顺序为左上，右上右下，左下
 %手动标记部分2：数独的左上角，黑线的外边缘
+
 pic_size = 28;
 margin = 10;
 box_size = pic_size+margin;
-img = imread(filename);
-
+%img = imread(filename);
+img = imread('IMG_1.JPG');
 
 
 %step 1 标出原数独图片的4个顶点
@@ -42,16 +43,19 @@ k1 = box_size * k + margin/2;
 X1 = X + pic_size;
 Y1 = Y + pic_size;
 figure;
-imshow(BW);
+imshow(img_gray);
 hold on;
 plot(X,Y,"r*")
 plot(X1,Y1,"G*")
 B = [X(:),Y(:),(pic_size-1)*ones(1,81)',(pic_size-1)*ones(1,81)'];
-digit_pics = zeros(81,pic_size,pic_size);
+digit_pics = zeros(pic_size,pic_size,81);
 for i=1:81
-    tmp = imcrop(BW,B(i,:));
-    %tmp = rgb2gray(tmp);
-    digit_pics(i,:,:) = reshape(tmp(:),pic_size,pic_size);
+    tmp = imcrop(img_gray,B(i,:));
+    %tmp = rgb2gray(tmp)
+    if i ==1
+        display((im2double(reshape(tmp(:),pic_size,pic_size))));
+    end
+    digit_pics(:,:,i) = 1 - im2double(reshape(tmp(:),pic_size,pic_size));
 end
 
 %imshow(reshape(digit_pics(10,:),28,28))调用方法
@@ -59,7 +63,7 @@ end
 %step 5 判断是否存在空格子
 is_empty = zeros(1,81);
 for i = 1:81
-    if sum(digit_pics(i,:)) > pic_size * pic_size * 0.95
+    if sum(sum(adaptivethresh(digit_pics(:,:,i)))) > pic_size * pic_size * 0.95
         is_empty(i) = 1;
     end
 end
