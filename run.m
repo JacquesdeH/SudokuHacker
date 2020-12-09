b861a24a4b8c400af6982147ec5ac26542f4135c
JacquesdeH <jacquesdeh@buaa.edu.cn>
2020-12-09 12:42:18 +0800

    Complete main staged

--- run.m
+++ run.m
@@ -1,12 +1,19 @@
 clear all;
 % load pics to workspace
-[digit_pics, is_empty] = img2nums('image/IMG_1.JPG');
+[digit_pics, is_empty] = img2nums_gray('image/IMG_1.JPG');
 % digit_pics = ones(28, 28, 81);
-reshaped_pics = reshape(digit_pics, 784, 81);
-load net120.mat
-load net28.mat
-outs = sim(net28, reshaped_pics);
-[maxout, preds] = max(outs);
+% predict model output
+load convnet_model.mat
+preds = zeros(1, 81);
+for k=1:81
+    cur_img = digit_pics(:,:,k);
+    cur_min = min(min(cur_img));
+    new_img = round((cur_img-cur_min)./(256-cur_min).*256);
+    out = predict(convnet, new_img);
+    [~, pred] = max(out);
+    pred = pred - 1;
+    preds(1, k) = pred;
+end
 preds = uint32(reshape(preds, 9, 9));
 % use dlx algorithm by first outlining zeros from original pic
 empty_mask = uint32(~is_empty);
