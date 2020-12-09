1618b66682e8f7aee25befef4908f05bf9e0eb05
张珈源 <1664420032@buaa.edu.cn>
2020-12-09 17:16:06 +0800

    input&output

--- Index_Sudo.m
+++ Index_Sudo.m
@@ -4,8 +4,8 @@ function [rowIndex,colIndex] = Index_Sudo(row, col)
 % 从在格子中的位置信息得到在矩阵中的信息
     wide = 4;   % 粗线
     thin = 2;   % 细线
-    gezi = 32;  % 格子宽度
-    liubai = 2; % 格子宽度与图像之间的差距
+    gezi = 44;  % 格子宽度
+    liubai = 8; % 格子宽度与图像之间的差距
     rowIndex = wide+(gezi+thin)*(row-1)+liubai;
     colIndex = wide+(gezi+thin)*(col-1)+liubai;
 end
